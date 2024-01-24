20147414fdd302047b441bd7f4a8b1841292c367
Pabatso Naketsana <patsokg@gmail.com>
2024-01-24 11:10:05 -0800

    Squared simple

--- 0x04-python-more_data_structures/0-main.py
+++ 0x04-python-more_data_structures/0-main.py
@@ -6,3 +6,12 @@ matrix = [
     [4, 5, 6],
     [7, 8, 9]
 ]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
+
+guillaume@ubuntu:~/0x04$ ./0-main.py
+[[1, 4, 9], [16, 25, 36], [49, 64, 81]]
+[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+guillaume@ubuntu:~/0x04$ 
